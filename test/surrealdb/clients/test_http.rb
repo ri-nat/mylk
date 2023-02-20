6c14312658ecea23be87c20a1f21d900d6e97a99
Rinat Shay <mail@rinatshay.com>
2023-02-20 22:39:37 +0200

    Setup `test` workflow (#1)

--- test/surrealdb/clients/test_http.rb
+++ test/surrealdb/clients/test_http.rb
@@ -3,19 +3,19 @@
 require "test_helper"
 
 class TestSurrealDB < Minitest::Test
-  DATABASE_URI = "http://root:root@localhost:8808/test/test"
+  DATABASE_URL = ENV.fetch("DATABASE_URL", "http://root:root@localhost:8000/test/test")
 
   def test_it_connects_to_surrealdb
-    assert SurrealDB::Clients::HTTP.new(DATABASE_URI).connect
+    assert SurrealDB::Clients::HTTP.new(DATABASE_URL).connect
   end
 
   def test_it_executes_successfully
-    client = SurrealDB::Clients::HTTP.new(DATABASE_URI).tap(&:connect)
+    client = SurrealDB::Clients::HTTP.new(DATABASE_URL).tap(&:connect)
 
     assert client.execute("INFO FOR DB").success?
   end
 
   def test_it_connects_automatically_on_execute
-    assert SurrealDB::Clients::HTTP.new(DATABASE_URI).execute("INFO FOR DB").success?
+    assert SurrealDB::Clients::HTTP.new(DATABASE_URL).execute("INFO FOR DB").success?
   end
 end
